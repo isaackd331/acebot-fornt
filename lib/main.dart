613e7a7cde07040d315e3847ae70d24f5045e935
nextinnovation-keedong <keedong331@nextinnovation.kr>
2024-05-14 15:36:52 +0900

    feat : flutter_native_splash 패키지 정상 구동 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:acebot_front/presentation/widget/common/BaseAppBar.dart';
 import 'package:acebot_front/presentation/widget/common/BaseDialog.dart';
 import 'package:acebot_front/presentation/widget/common/BaseOutlineButton.dart';
 
-void main() => runApp(MaterialApp(
-    home: const AceBot(),
-    theme: ThemeData(
-      fontFamily: "Pretendard",
-    )));
+void main() {
+  /**
+   * Splash Image 관련 코드
+   * Splash Image를 특정 행위가 종료되기 전까지 유지시킬 수 있음
+   */
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(MaterialApp(
+      home: const AceBot(),
+      theme: ThemeData(
+        fontFamily: "Pretendard",
+      )));
+}
 
 class AceBot extends StatelessWidget {
   const AceBot({super.key});
