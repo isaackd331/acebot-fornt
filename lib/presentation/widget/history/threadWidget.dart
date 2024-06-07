c8326813b0d4405f62dbea1db7ec09aac825a436
Isaac_KD <isaackd331@gmail.com>
2024-06-07 10:18:31 +0900

    chore : 스레드가 없을 시 "스레드 추가하기" 버튼 누를 시 이벤트 추가

--- lib/presentation/widget/history/threadWidget.dart
+++ lib/presentation/widget/history/threadWidget.dart
@@ -4,6 +4,7 @@ library;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:acebot_front/presentation/widget/common/baseOutlineButton.dart';
 
@@ -28,6 +29,53 @@ class _ThreadWidgetState extends State<ThreadWidget> {
     super.dispose();
    }
 
+   Widget _loadedButEmpty() {
+    return Expanded(
+      child: Column(
+        children: [
+          Flexible(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 68,
+                  height: 68,
+                  child: Image.asset('assets/icons/icon_thread_empty.png')
+                ),
+                const SizedBox(height: 12),
+                const Text('스레드가 없습니다.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff1c1c1c)
+                  )
+                )
+              ]
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    BaseOutlineButton(onPressedFunc: () {
+                      context.go('/home');
+                    }, text: '스레드 추가하기', fontSize: 16, textColor: const Color(0xffffffff), backgroundColor: const Color(0xff000000), borderColor: const Color(0xff000000))
+                  ]
+                ),
+                const SizedBox(height: 24)
+              ]
+            )
+          )
+        ]
+      )
+    );
+   }
+
    @override
    Widget build(BuildContext context) {
     return BlocBuilder<ThreadCubit, ThreadState>(
@@ -36,50 +84,28 @@ class _ThreadWidgetState extends State<ThreadWidget> {
           return Container();
         } else {
           if(state.threadJson.items.isEmpty) {
-            return Expanded(
-              child: Column(
-                children: [
-                  Flexible(
-                    flex: 4,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 68,
-                          height: 68,
-                          child: Image.asset('assets/icons/icon_thread_empty.png')
-                        ),
-                        const SizedBox(height: 12),
-                        const Text('스레드가 없습니다.',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xff1c1c1c)
-                          )
-                        )
-                      ]
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            BaseOutlineButton(onPressedFunc: () {}, text: '스레드 추가하기', fontSize: 16, textColor: const Color(0xffffffff), backgroundColor: const Color(0xff000000), borderColor: const Color(0xff000000))
-                          ]
-                        ),
-                        const SizedBox(height: 24)
-                      ]
-                    )
-                  )
-                ]
-              )
-            );
+            return _loadedButEmpty();
           } else {
-            return Container();
+            return Column(
+              children: [
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text('편집',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff666666)
+                        )
+                      )
+                    )
+                  ]
+                )
+              ]
+            );
           }
         }
       }
