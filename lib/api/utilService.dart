5fef9a57820b63e618808b7c8067929797dde5a7
Isaac_KD <isaackd331@gmail.com>
2024-06-25 08:53:23 +0900

    feat : html 요소 렌더링을 위한 flutter_html 패키지 설치

---
+++ lib/api/utilService.dart
@@ -0,0 +1,20 @@
+import 'package:acebot_front/api/http.dart';
+import 'package:dio/dio.dart';
+
+class UtilService {
+  getHomePrompts() {
+    return dio.get("/prompts");
+  }
+
+  getWorksRolesList() {
+    return dio.get("/roles-works");
+  }
+
+  getPrivacyPolicy() {
+    return dio.get("/terms/privacy");
+  }
+
+  getServicePolicy() {
+    return dio.get("/terms/service");
+  }
+}
