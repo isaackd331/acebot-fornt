dc1879ca60f5df908ef7141da17e8fdcda0f0864
Isaac_KD <isaackd331@gmail.com>
2024-07-09 07:59:11 +0900

    feat : 회원가입 서비스 생성

--- lib/api/userService.dart
+++ lib/api/userService.dart
@@ -10,7 +10,12 @@ class UserService {
     return dio.get("/v1/users", options: Options(headers: {'email': userId}));
   }
 
-  // 검증
+  /// 검증
+  /// email: 가입 이메일 검증
+  /// activated: 이메일 활성 코드 검증
+  /// send-activated: 활성 코드 발송
+  /// password: 비밀번호 재설정 코드 검증
+  /// send-password: 비밀번호 재설정 코드 발송
   verify(String email, String? value, String type) {
     Map<String, dynamic> data;
 
@@ -21,5 +26,10 @@ class UserService {
     }
 
     return dio.post("/v1/users/verification/$type", data: data);
+  }
+
+  // 가입
+  join(dynamic params) {
+    return dio.post("/v1/users", data: params);
   }
 }
